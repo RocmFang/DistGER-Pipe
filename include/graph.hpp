4b36cc9191be5fa575481591d0a30bb61851e305
lzl <ztruth@qq.com>
2023-11-08 21:31:01 +0800

    [Fixup] fix int parameter overflow in MPI_Isend, Graph.hpp

--- include/graph.hpp
+++ include/graph.hpp
@@ -1143,9 +1143,26 @@ public:
                 MPI_Request* req = new MPI_Request();
                 requests.push_back(req);
                 // std::cout << "diff * sizeof(msg_t) = " << diff << " " << sizeof(msg_t) << std::endl;
-                assert(diff * sizeof(msg_t) < INT_MAX);
-        
-                MPI_Isend(((msg_t*)msg_send_buffer[dst]->data) + send_progress[dst], diff * sizeof(msg_t), get_mpi_data_type<char>(), dst, Tag_Msg, MPI_COMM_WORLD, req);
+                unsigned long long msg_size = (unsigned long long)diff * sizeof(msg_t);
+                if (msg_size > INT_MAX)
+                {
+                    // printf("溢出了\n");
+                    unsigned long long package_size = 1024 * 1024 * 100;
+                    int round = msg_size / package_size + ((msg_size % package_size) > 0 ? 1 : 0);
+                    for (int r = 0; r < round; r++)
+                    {
+                        unsigned long long start = r * package_size;
+                        int send_size = min(package_size, (msg_size - start));
+                        assert(send_size < INT_MAX);
+                        // printf("send_size: %d\n",send_size);
+                        MPI_Isend(((msg_t *)msg_send_buffer[dst]->data) + send_progress[dst] + start, send_size, get_mpi_data_type<char>(), dst, Tag_Msg, MPI_COMM_WORLD, req);
+                    }
+                }
+                else
+                {
+                    assert(diff * sizeof(msg_t) < INT_MAX);
+                    MPI_Isend(((msg_t *)msg_send_buffer[dst]->data) + send_progress[dst], diff * sizeof(msg_t), get_mpi_data_type<char>(), dst, Tag_Msg, MPI_COMM_WORLD, req);
+                }
 #ifdef PERF_PROF
                 if (local_partition_id == 0)
                 {
